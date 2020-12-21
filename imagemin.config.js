679a9da485a226cc44958dd1ac08cb6b3e320eb6
rogermedico <roger.medico@gmail.com>
2020-12-21 12:58:04 +0100

    imagemin jpg

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
-  "mozjpeg": { "progressive": true, "quality": 10 },
+  "mozjpeg": { "progressive": true, "quality": 100 },
   "pngquant": { "quality": [0.25, 0.5] },
   "svgo": {
     "plugins": [
